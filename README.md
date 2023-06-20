c91c95a7893f9cc46175f3a309236a801519c7c4
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2023-06-20 05:55:38 +0530

    Add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+### SCS2204 Functional Programming - Tutorial 02 submission
+
+- Index No: 21000158
+- Name: A.V.S.R.ATIGALA
+
+GitHub repositary : https://github.com/ucsc-projects/FP-LAB2-21000158.git
--- src/main/scala/Q1.scala
+++ src/main/scala/Q1.scala
@@ -1,4 +1,16 @@
 object Q1 {
+  def main(args: Array[String]){
+    var k=2;
+    var i=2;
+    var j=2;
+    var m=5;
+    var n=5;
+    var f=12.0f;
+    var g=4.0f;
+    var c='X';
 
+    println()
+
+  }
 
 }
