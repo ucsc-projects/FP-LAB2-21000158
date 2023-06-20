f64009f22b504d453cc58c4049ea726984a1e296
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2023-06-20 08:12:42 +0530

    Finish Question 1

--- src/main/scala/Q1.scala
+++ src/main/scala/Q1.scala
@@ -1,15 +1,21 @@
 object Q1 {
-  def main(args: Array[String]){
-    var k=2;
-    var i=2;
-    var j=2;
-    var m=5;
-    var n=5;
-    var f=12.0f;
-    var g=4.0f;
-    var c='X';
+  def main(args: Array[String]) {
+    var k = 2;
+    var i = 2;
+    var j = 2;
+    var m = 5;
+    var n = 5;
+    var f = 12.0f;
+    var g = 4.0f;
+    var c = 'X';
 
-    println()
+    println(k + 12 * m);
+    println(m / j);
+    println(n % j);
+    println(m / j * j);
+    println(f + 10 * 5 + g);
+    //    println(++i*n);
+
 
   }
 
